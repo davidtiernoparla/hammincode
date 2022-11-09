8019de5d248915165c518e4bf2ef41e7118fced7
david ortega <ortegahoyas.david@tiernoparla.es>
2022-11-09 18:13:03 +0100

    Se amplia la clase Main usando metodos de Writer

--- hammingcode/src/Main.java
+++ hammingcode/src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Writer escritor = new Writer();
+
+        int[] mensaje = escritor.generarMensaje();
+
+        System.out.println("");
+        System.out.println("mensaje en bruto, sin aplicar hamming:  ");
+        System.out.println("");
+
+        for (int i = 0; i < mensaje.length; i++) {
+        System.out.print(mensaje[i]);
+        }
     }
 }
