60c4a211f855da2f37223ade0e3777dac4ea116b
davidtiernoparla <ortegahoyas.david@tiernoparla.es>
2022-11-22 13:54:24 +0100

    Incluido el uso de metodos de la clase Sender para ampliar la clase Main

--- hammingcode/src/Main.java
+++ hammingcode/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Writer escritor = new Writer();
+        Sender mensajero = new Sender();
 
         int[] mensaje = escritor.generarMensaje();
 
@@ -11,5 +12,13 @@ public class Main {
         for (int i = 0; i < mensaje.length; i++) {
         System.out.print(mensaje[i]);
         }
+        mensaje = mensajero.generarMensajeConHamming(mensaje, mensaje.length);
+        System.out.println("");
+        System.out.println("Mensaje de hamming con todos los bits de redundancia:  ");
+        System.out.println("");
+        for (int i = 0; i < mensaje.length; i++) {
+            System.out.print(mensaje[i]);
+        }
+        
     }
 }
