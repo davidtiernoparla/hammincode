0f249474ad7d86ff883ddc03fafe2f96993b18a8
david ortega <ortegahoyas.david@tiernoparla.es>
2022-11-23 20:50:23 +0100

    Se expande la clase Main con metodos de Noise

--- hammingcode/src/Main.java
+++ hammingcode/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Writer escritor = new Writer();
         Sender mensajero = new Sender();
+        Noise ruido = new Noise();
 
         int[] mensaje = escritor.generarMensaje();
 
@@ -10,14 +11,33 @@ public class Main {
         System.out.println("");
 
         for (int i = 0; i < mensaje.length; i++) {
+
         System.out.print(mensaje[i]);
+
         }
+
         mensaje = mensajero.generarMensajeConHamming(mensaje, mensaje.length);
+
         System.out.println("");
         System.out.println("Mensaje de hamming con todos los bits de redundancia:  ");
         System.out.println("");
+
         for (int i = 0; i < mensaje.length; i++) {
+
             System.out.print(mensaje[i]);
+
+        }
+        
+        mensaje = ruido.alterarMensaje(mensaje);
+
+        System.out.println("");
+        System.out.println("Mensaje de hamming con 0, 1 o 2 errores:  ");
+        System.out.println("");
+
+        for (int i = 0; i < mensaje.length; i++) {
+
+            System.out.print(mensaje[i]);
+
         }
         
     }
